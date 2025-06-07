176d769d9d6fadb6473d45ab7b76aa98230fd1c0
Стас Затушевский <STAs20160@yandex.ru>
2025-06-07 21:11:08 +0300

    test

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -3,15 +3,25 @@ package routers
 import (
 	"cutURL/internal/handlers"
 	"cutURL/internal/storage"
-	"github.com/go-chi/chi/v5"
+	"github.com/gin-gonic/gin"
 )
 
-func RouterNew() *chi.Mux {
-	r := chi.NewRouter()
+//func RouterNew() *chi.Mux {
+//	r := chi.NewRouter()
+//	s := storage.NewStorage()
+//
+//	r.Post("/", handlers.CreateURLHandler(s))
+//	r.Get("/{id}", handlers.GetURLHandler(s))
+//
+//	return r
+//}
+
+func RouterNew() *gin.Engine {
+	r := gin.Default()
 	s := storage.NewStorage()
 
-	r.Post("/", handlers.CreateURLHandler(s))
-	r.Get("/{id}", handlers.GetURLHandler(s))
+	r.POST("/", handlers.CreateURLHandler(s))
+	r.GET("/:id", handlers.GetURLHandler(s))
 
 	return r
 }
--- internal/urlshortener/gzip_test.go
+++ internal/urlshortener/gzip_test.go
@@ -1,1 +1,19 @@
 package urlshortener
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestURLBuilder(t *testing.T) {
+	wantURL := "http://localhost:8080"
+	wantLength := 8
+	builder := NewURLBuilder(wantLength)
+	newURL := builder.CreateURL()
+	if strings.HasPrefix(newURL, wantURL) != true {
+		t.Errorf("URLBuilder.CreateURL() = %v, want = %v", newURL, wantURL)
+	}
+	if len([]rune(builder.StringID)) != 8 {
+		t.Errorf("Wrong lenght of StringID = %v, want = %v", len(newURL), wantLength)
+	}
+}
